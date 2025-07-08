378e73c233278afddeba4ba4ece308bedaf6733e
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-07-08 16:21:42 +0200

    ref: standardize parameter naming from 'channel' to 'room_id' in CommandHandler methods

--- matrixbot/commands.py
+++ matrixbot/commands.py
@@ -8,29 +8,30 @@ class CommandHandler:
         self.config = config
         self.logger = logger
 
-    async def handle_ai(self, channel, message_parts, sender):
+    async def handle_ai(self, room_id, message_parts, sender):
         prompt = " ".join(message_parts[1:])
-        await self.history_manager.add("user", channel, prompt, sender)
+        await self.history_manager.add("user", room_id, prompt, sender)
         self.logger.info(f"Querying rag with prompt: {prompt}")
-        chat_history = self.history_manager.get(channel, sender)
+        chat_history = self.history_manager.get(room_id, sender)
         response = await self.rag_service.query_model(
             prompt, chat_history, self.logger
         )
-        await self.history_manager.add("assistant", channel, response, sender)
-        await self.matrix_client.send_message(channel, response)
+        await self.history_manager.add("assistant", room_id, response, sender)
+        await self.matrix_client.send_message(room_id, response)
 
-    async def handle_reset(self, channel, sender):
-        await self.history_manager.reset(channel, sender)
+    async def handle_reset(self, room_id, sender):
+        await self.history_manager.reset(room_id, sender)
         await self.matrix_client.send_message(
-            channel, f"@{sender} conversation history reset."
+            room_id, f"{sender} conversation history reset."
         )
 
-    async def handle_help(self, channel):
+    async def handle_help(self, room_id):
         help_message = """
-        🤖 **Available commands:**\n
-        .ai <prompt> - Generate AI response.\n
+        🤖 Available commands:\n
+        -------------------\n
+        <prompt> - Generate AI response.\n
         .reset - Reset conversation.\n
         .help - Show help.\n
         """
 
-        await self.matrix_client.send_message(channel, help_message)
+        await self.matrix_client.send_message(room_id, help_message)
