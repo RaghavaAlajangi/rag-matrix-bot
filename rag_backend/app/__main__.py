d8443b8d521abb6d54af6ffab8cad755a994ea29
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-08-28 18:07:00 +0200

    ref: move app instataniation to cli.py

--- rag_backend/app/__main__.py
+++ rag_backend/app/__main__.py
@@ -1,32 +1,4 @@
-import click
-import uvicorn
-from fastapi import FastAPI
-
-from .api import rag
-
-app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return {"RAGbot health status": "Running!"}
-
-
-# Include the RAG router
-app.include_router(rag.router)
-
-
-@click.command()
-@click.option("--port", default=8000, help="Port for the FastAPI app.")
-@click.option("--local", is_flag=True, help="Run app locally.")
-def serve(port=8000, local=False):
-    """Run the rag_backend app with specified options."""
-    if not local:
-        host = "0.0.0.0"  # Host IP address
-    else:
-        host = "127.0.0.1"
-    uvicorn.run(app, log_level="info", port=port, host=host)
-
+from .cli import serve
 
 if __name__ == "__main__":
     serve()
