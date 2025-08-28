bec5ca63e1ac25f92c6c4208bb809f87013e1693
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-08-28 16:07:04 +0200

    enh: make sure rag_backend listens correct interface

--- rag_backend/app/__main__.py
+++ rag_backend/app/__main__.py
@@ -1,3 +1,4 @@
+import click
 import uvicorn
 from fastapi import FastAPI
 
@@ -15,5 +16,17 @@ def root():
 app.include_router(rag.router)
 
 
+@click.command()
+@click.option("--port", default=8000, help="Port for the FastAPI app.")
+@click.option("--local", is_flag=True, help="Run app locally.")
+def serve(port=8000, local=False):
+    """Run the rag_backend app with specified options."""
+    if not local:
+        host = "0.0.0.0"  # Host IP address
+    else:
+        host = "127.0.0.1"
+    uvicorn.run(app, log_level="info", port=port, host=host)
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, log_level="info", port=1050)
+    serve()
