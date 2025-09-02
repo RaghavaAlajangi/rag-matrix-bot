9b646662726b48fc4a9dbcdba1cecea4293d51b1
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-09-02 10:51:13 +0200

    ref: use a single env file for simplicity

--- ingest_data/qdrant_ingest.py
+++ ingest_data/qdrant_ingest.py
@@ -8,7 +8,7 @@ from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams
 
-load_dotenv()
+load_dotenv("./.env")
 
 embedder = OpenAIEmbeddings(
     model=os.getenv("OPENAI_EMBEDDING_MODEL_NAME"),
